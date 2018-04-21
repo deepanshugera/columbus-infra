881ded4ac95986aa7f899041ce473c3b5b830819
Sugadev <sugadev@Sugadevs-MacBook-Pro.local>
2018-04-21 17:45:10 -0500

    added upload file feature

--- columbus/interface/views.py
+++ columbus/interface/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import CodeForm
 # Create your views here.
 from django.http import HttpResponse
+from django.core.files.storage import FileSystemStorage
 
 def hello(request):
 
@@ -10,8 +11,14 @@ def hello(request):
 
 def upload_file(request):
    #file upload
-   user_code=""
-   return render(request,"interface/command.html",{"user_code":user_code})
+   user_code = ""
+   if request.method == 'POST' and request.FILES['csv_file']:
+      csv_file = request.FILES['csv_file']
+      fs = FileSystemStorage()
+      filename = fs.save(csv_file.name, csv_file)
+      uploaded_file_url = fs.url(filename)
+      return render(request, "interface/command.html", {"user_code": user_code,'success': uploaded_file_url+" uploaded successfully"})
+   return render(request,"interface/home.html",{"error":"Unable to upload file"})
 
 def exec_command(request):
 
