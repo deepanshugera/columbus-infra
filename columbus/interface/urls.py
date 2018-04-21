f3e916ab1bc52809977b5f4beb4ac2c5a2e6d7ab
Sugadev <sugadev@Sugadevs-MacBook-Pro.local>
2018-04-21 13:24:48 -0500

    add home page

--- columbus/interface/urls.py
+++ columbus/interface/urls.py
@@ -1,4 +1,4 @@
-"""columbus URL Configuration
+"""myproject URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include,path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', include('interface.urls')),
+    path('', views.hello,name='hello'),
 ]
